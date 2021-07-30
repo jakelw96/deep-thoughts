5e644c8931537bea208b80ac8ac527ceb3557420
jakelw96 <jake.lw96@gmail.com>
2021-07-30 12:34:38 -0600

    Started front-end, connected to back-end

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
@@ -20,6 +21,15 @@ async function startApolloServer() {
   
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
+
+  // Serve up the static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
   
   db.once('open', () => {
     app.listen(PORT, () => {
